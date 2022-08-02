b7a9e8bdd997b704a2e7e67d419a613379c9b8a5
mugiisha <mugishabankz3@gmail.com>
2022-08-02 22:10:48 +0200

    add todo list model and relationships with todo model

--- app.py
+++ app.py
@@ -15,13 +15,59 @@ class Todo(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     description=db.Column(db.String(),nullable=False)
     completed=db.Column(db.Boolean(),default=False,nullable=False)
-
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
-@app.route('/')
-def index():
-   return  render_template('index.html', data=Todo.query.all())
+class TodoList(db.Model):
+    __tablename__='todolists'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(),nullable=False)
+    todos = db.relationship('Todo',backref='list',lazy=True)
+
+
+
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+   return  render_template('index.html',
+   lists=TodoList.query.all(),
+   active_list=TodoList.query.get(list_id)
+   , todos=Todo.query.filter_by(list_id=list_id).order_by('id').all())
+
+@app.route('/lists/create', methods=['POST'])
+def create_list():
+    error = False
+    body={}
+    try:
+        list_name=request.get_json()['name']
+        list=TodoList(name=list_name)
+        db.session.add(list)
+        db.session.commit()
+        body['name']=list.name
+    except:
+        error=True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if(error):
+        abort (400)
+    else:
+        return jsonify(body)
+
+@app.route('/lists/<list_id>/delete-list', methods=['DELETE'])
+def delete_list(list_id):
+    try:
+        todo = TodoList.query.get(list_id)
+        print(todo)
+        db.session.delete(todo)
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
 
 @app.route('/todos/create', methods=['POST'])
 def createtodo():
@@ -71,6 +117,9 @@ def delete_todo(todo_id):
     return redirect(url_for('index'))
 
 
+@app.route('/')
+def index():
+    return redirect(url_for('get_list_todos', list_id=1))
 
 
 
